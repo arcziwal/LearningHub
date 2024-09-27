77b0423d71dbc48e7c9e242ea9b3c425d30189ab
Artur <waligoraartur95@gmail.com>
2024-09-27 18:16:05 +0200

    main loop established, words counting implemented, example text file included

--- BasicPython/Projects/text_analyser.py
+++ BasicPython/Projects/text_analyser.py
@@ -1,5 +1,7 @@
 import re
 import sys
+from os.path import split
+
 
 def text_normalizer(text_to_normalize):
     text_to_normalize = text_to_normalize.lower()
@@ -21,33 +23,26 @@ def load_text_from_file():
             else:
                 continue
 
-def main_menu():
-    border = "*" * 50
-    welcome_text = "Witaj w Analizatorze tekstu. Wybierz odpowiednią opcję:"
-    menu = """
+def text_input():
+    text_input_options = """Wybierz odpowiednią opcję wprowadzenia tekstu:
     1. Wpisz tekst do analizy
     2. Wczytaj tekst do analizy z pliku txt
-    3. Wyjście z programu
-    """
-    full_message = f"{border}\n{welcome_text}\n{border}\n{menu}\n"
-    decision = input(full_message)
+    3. Wyjście z programu"""
+    decision = input(text_decorator(text_input_options))
     return decision
 
-
-
-
-if __name__ == "__main__":
+def text_loader():
     while True:
-        decision = main_menu()
+        decision = text_input()
         if decision == "1":
-            text = input("Wpisz poniżej swój tekst do przeanalizowania:\n")
+            text = input("Wpisz poniżej tekst do analizy:\n")
             normalized_text = text_normalizer(text)
-            print(f"Twój znormalizowany tekst to:\n{normalized_text}")
+            return normalized_text
         elif decision == "2":
             text = load_text_from_file()
             if text:
                 normalized_text = text_normalizer(text)
-                print(f"Twój znormalizowany tekst to:\n{normalized_text}")
+                return normalized_text
             else:
                 continue
         elif decision == "3":
@@ -55,4 +50,40 @@ if __name__ == "__main__":
         else:
             print("Nieprawidłowy wybór. Spróbuj ponownie")
             continue
+
+def text_decorator(text):
+    text = text.capitalize()
+    border = "*" * 50
+    return f"{border}\n{text}\n{border}\n"
+
+
+def text_analyse_menu():
+    text_analyse_options = """Aby przeanalizować tekst wybierz dostępne opcje:
+    1. Zlicz słowa
+    2. Załaduj nowy tekst
+    3. Wyjście z programu"""
+    decision = input(text_decorator(text_analyse_options))
+    return decision
+
+def count_words(text):
+    text = text.split()
+    words_count = len(text)
+    return words_count
+
+
+
+if __name__ == "__main__":
+    text_to_analyse = text_loader()
+    while True:
+        user_decision = text_analyse_menu()
+        if user_decision == "1":
+            words_number = count_words(text_to_analyse)
+            print(f"\nTwój tekst zawiera {words_number} słów.")
+            continue
+        elif user_decision == "2":
+            text_to_analyse = text_loader()
+            continue
+        elif user_decision == "3":
+            sys.exit("Dziękuję za użycie Analizatora Tekstu. Miłego dnia!")
+
 
